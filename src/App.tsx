24da88342fc688aceadeb0fdbbbb4a5d4d47acab
Amrit Timalsina <amrittimalsina10@gmail.com>
2024-05-04 15:16:56 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Projects from "./components/Projects";
+// import Todos from "./components/Todos";
+// import Products from "./components/Products";
+
+function App() {
+  return (
+    <>
+      {/* <Todos /> */}
+      <Projects />
+      {/* <Products /> */}
+    </>
+  );
+}
+
+export default App;
